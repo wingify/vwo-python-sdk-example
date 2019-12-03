ba61dde8222b965bc8f23169608810bc1274b9fd
softvar <varun2902@gmail.com>
2019-12-03 13:53:01 +0530

    feat(sdk-example): pre and post demo capabilites

--- config.py
+++ config.py
@@ -1,16 +1,54 @@
-account_id = ''
-sdk_key = ''
+# Copyright 2019 Wingify Software Pvt. Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-ab_campaign_key = ''
-ab_campaign_goal_identifier = ''
+AccountDetails = {
+    'account_id': '',
+    'sdk_key': '',
+}
 
-feature_rollout_campaign_key = ''
+AbCampaignData = {
+    'campaign_key': '',
+    'campaign_goal_identifier': '',
+    'user_id': '',
+    'revenue_value': '',
+    # custom_variables: Optional param, used for pre-segmentation
+    # 'custom_variables': {}
+}
 
-feature_campaign_key = ''
-feature_campaign_goal_identifier = ''
-feature_variable_key = ''
+FeatureRolloutData =  {
+    'campaign_key': '',
+    'user_id': '',
+    # custom_variables: Optional param, used for pre-segmentation
+    # 'custom_variables': {}
+}
 
-string_variable_key = ''
-integer_variable_key = ''
-double_variable_key = ''
-boolean_variable_key = ''
+FeatureTestData = {
+    'campaign_key': '',
+    'campaign_goal_identifier': '',
+    'revenue_value': '',
+    'user_id': '',
+    # custom_variables: Optional param, used for pre-segmentation
+    # 'custom_variables': {}
+
+    'string_variable_key': '',
+    'integer_variable_key': '',
+    'double_variable_key': '',
+    'boolean_variable_key': '',
+}
+
+PushData = {
+    'tag_key': '',
+    'tag_value': '',
+    'user_id': '',
+}
--- server.py
+++ server.py
@@ -1,17 +1,36 @@
-from __future__ import print_function
-import vwo
-from vwo import UserStorage, LogLevels
+# Copyright 2019 Wingify Software Pvt. Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-from config import *
+
+from __future__ import print_function
 
 import json
 import threading
 
-from util import get_random_user
+import vwo
 from flask import Flask, jsonify, render_template, request
-app = Flask(__name__)
+from vwo import LogLevels, UserStorage, logger
 
-from vwo import logger
+try:
+    from dev_config import (AbCampaignData, AccountDetails, FeatureRolloutData,
+                    FeatureTestData, PushData)
+except:
+    from config import (AbCampaignData, AccountDetails, FeatureRolloutData,
+                    FeatureTestData, PushData)
+from util import get_random_user
+
+app = Flask(__name__)
 
 def set_interval(func, sec):
     def func_wrapper():
@@ -43,9 +62,12 @@ def init_sdk():
     global vwo_client_instance
     global settings_file
 
-    # new_settings_file = vwo.get_settings_file(account_id, sdk_key)
-    with open('test_settings_file.json') as json_file:
-        new_settings_file = json.dumps(json.load(json_file))
+    new_settings_file = vwo.get_settings_file(AccountDetails.get('account_id'), AccountDetails.get('sdk_key'))
+
+    # Uncomment below code to read settings_file from local settingsFile.json
+
+    # with open('test_settings_file.json') as json_file:
+    #     new_settings_file = json.dumps(json.load(json_file))
 
     print('settingsFile fetched')
 
@@ -65,31 +87,34 @@ def init_sdk():
 init_sdk()
 # set_interval(init_sdk, POLL_TIME)
 
+
 @app.route('/')
 def index():
     return render_template('homepage.html')
 
+
 @app.route('/ab')
 def ab_campaign():
-    print(ab_campaign_key)
     req_user_id = request.args.get('userId')
 
     if req_user_id == None:
-        user_id = get_random_user()
+        user_id = AbCampaignData.get('user_id') or get_random_user()
     else:
         user_id = str(req_user_id)
 
-    print(user_id)
-
-    variation_name = vwo_client_instance.activate(ab_campaign_key, user_id)
+    variation_name = vwo_client_instance.activate(AbCampaignData.get('campaign_key'),
+                                                  user_id,
+                                                  custom_variables=AbCampaignData.get('custom_variables'))
 
     if variation_name:
       is_part_of_campaign = True
     else:
       is_part_of_campaign = False
 
-    revenue_value = 1234
-    vwo_client_instance.track(ab_campaign_key, user_id, ab_campaign_goal_identifier, revenue_value)
+    vwo_client_instance.track(AbCampaignData.get('campaign_key'),
+                              user_id,
+                              AbCampaignData.get('campaign_goal_identifier'),
+                              revenue_value=AbCampaignData.get('revenue_value'))
 
     return render_template(
         'ab.html',
@@ -97,68 +122,107 @@ def ab_campaign():
         campaign_type = "Visual-AB",
         is_part_of_campaign = is_part_of_campaign,
         variation_name = variation_name,
-        ab_campaign_key = ab_campaign_key,
-        ab_campaign_goal_identifier = ab_campaign_goal_identifier,
+        ab_campaign_key = AbCampaignData.get('campaign_key'),
+        ab_campaign_goal_identifier = AbCampaignData.get('campaign_goal_identifier'),
+        custom_variables = json.dumps(AbCampaignData.get('custom_variables'), sort_keys = True, indent = 4, separators = (',', ': ')),
         settings_file = json.dumps(json.loads(settings_file), sort_keys = True, indent = 4, separators = (',', ': '))
     )
 
+
 @app.route('/feature-rollout')
 def feature_rollout_campaign():
-    print(feature_rollout_campaign_key)
     req_user_id = request.args.get('userId')
 
     if req_user_id == None:
-        user_id = get_random_user()
+        user_id = FeatureRolloutData.get('user_id') or get_random_user()
     else:
         user_id = str(req_user_id)
 
-    print(user_id)
-
-    is_user_part_of_feature_rollout_campaign = vwo_client_instance.is_feature_enabled(feature_rollout_campaign_key, user_id)
+    is_user_part_of_feature_rollout_campaign = vwo_client_instance.is_feature_enabled(FeatureRolloutData.get('campaign_key'),
+                                                                                      user_id,
+                                                                                      custom_variables=FeatureRolloutData.get('custom_variables'))
 
     return render_template(
         'feature-rollout.html',
         campaign_type = "Feature-rollout",
         user_id = user_id,
         is_user_part_of_feature_rollout_campaign = is_user_part_of_feature_rollout_campaign,
-        feature_rollout_campaign_key = feature_rollout_campaign_key,
+        feature_rollout_campaign_key = FeatureRolloutData.get('campaign_key'),
+        custom_variables = json.dumps(FeatureRolloutData.get('custom_variables'), sort_keys = True, indent = 4, separators = (',', ': ')),
         settings_file = json.dumps(json.loads(settings_file), sort_keys = True, indent = 4, separators = (',', ': '))
     )
 
+
 @app.route('/feature-test')
 def feature_campaign():
-    print(feature_campaign_key)
     req_user_id = request.args.get('userId')
 
     if req_user_id == None:
-        user_id = get_random_user()
+        user_id = FeatureTestData.get('user_id') or get_random_user()
     else:
         user_id = str(req_user_id)
 
-    print(user_id)
-    is_user_part_of_feature_campaign = vwo_client_instance.is_feature_enabled(feature_campaign_key, user_id)
-    print(is_user_part_of_feature_campaign)
-    revenue_value = 1234
-    vwo_client_instance.track(feature_campaign_key, user_id, feature_campaign_goal_identifier, revenue_value)
+    is_user_part_of_feature_campaign = vwo_client_instance.is_feature_enabled(FeatureTestData.get('campaign_key'), user_id)
 
-    string_variable = vwo_client_instance.get_feature_variable_value(feature_campaign_key, string_variable_key, user_id)
-    integer_variable = vwo_client_instance.get_feature_variable_value(feature_campaign_key, integer_variable_key, user_id)
-    boolean_variable = vwo_client_instance.get_feature_variable_value(feature_campaign_key, boolean_variable_key, user_id)
-    double_variable = vwo_client_instance.get_feature_variable_value(feature_campaign_key, double_variable_key, user_id)
+    vwo_client_instance.track(FeatureTestData.get('campaign_key'),
+                              user_id,
+                              FeatureTestData.get('campaign_goal_identifier'),
+                              revenue_value=FeatureTestData.get('revenue_value'),
+                              custom_variables=FeatureTestData.get('custom_variables'))
+
+    string_variable = vwo_client_instance.get_feature_variable_value(FeatureTestData.get('campaign_key'),
+                                                                     FeatureTestData.get('string_variable_key'),
+                                                                     user_id,
+                                                                     custom_variables=FeatureTestData.get('custom_variables'))
+    integer_variable = vwo_client_instance.get_feature_variable_value(FeatureTestData.get('campaign_key'),
+                                                                     FeatureTestData.get('integer_variable_key'),
+                                                                     user_id,
+                                                                     custom_variables=FeatureTestData.get('custom_variables'))
+    boolean_variable = vwo_client_instance.get_feature_variable_value(FeatureTestData.get('campaign_key'),
+                                                                     FeatureTestData.get('boolean_variable_key'),
+                                                                     user_id,
+                                                                     custom_variables=FeatureTestData.get('custom_variables'))
+    double_variable = vwo_client_instance.get_feature_variable_value(FeatureTestData.get('campaign_key'),
+                                                                     FeatureTestData.get('double_variable_key'),
+                                                                     user_id,
+                                                                     custom_variables=FeatureTestData.get('custom_variables'))
 
     return render_template(
         'feature-test.html',
         user_id = user_id,
         campaign_type = "Feature-test",
         is_user_part_of_feature_campaign = is_user_part_of_feature_campaign,
-        feature_campaign_key = feature_campaign_key,
-        feature_campaign_goal_identifier = feature_campaign_goal_identifier,
+        feature_campaign_key = FeatureTestData.get('campaign_key'),
+        feature_campaign_goal_identifier = FeatureTestData.get('campaign_goal_identifier'),
         string_variable = string_variable,
         integer_variable = integer_variable,
         boolean_variable = boolean_variable,
         double_variable = double_variable,
+        custom_variables = json.dumps(FeatureTestData.get('custom_variables'), sort_keys = True, indent = 4, separators = (',', ': ')),
         settings_file = json.dumps(json.loads(settings_file), sort_keys = True, indent = 4, separators = (',', ': '))
     )
+
+
+@app.route('/push')
+def push_api():
+    req_user_id = request.args.get('userId')
+
+    if req_user_id == None:
+        user_id = PushData.get('user_id') or get_random_user()
+    else:
+        user_id = str(req_user_id)
+
+    result = vwo_client_instance.push(PushData.get('tag_key'), PushData.get('tag_value'), user_id)
+
+    return render_template(
+        'push.html',
+        user_id = user_id,
+        tag_key = PushData.get('tag_key'),
+        tag_value = PushData.get('tag_value'),
+        result = result,
+        settings_file = json.dumps(json.loads(settings_file), sort_keys = True, indent = 4, separators = (',', ': '))
+    )
+
 
 if __name__ == '__main__':
     app.run(debug = True)
